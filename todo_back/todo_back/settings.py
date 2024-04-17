b2aaabc5d10580df83ec5b0ca81d74b8307f7e88
Mishan Shah <smishan7@gmail.com>
2024-04-17 19:29:28 +0545

    correct spell of backend in installed apps

--- todo_back/todo_back/settings.py
+++ todo_back/todo_back/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend',
+    'Backend',
     'corsheaders',
     'rest_framework',
     'Authentication',
