88e20b82c6053653d53735dcd8ae20a15f814c7e
Mishan Shah <smishan7@gmail.com>
2024-04-18 11:20:10 +0545

    No module named 'Description'

--- todo_back/todo_back/urls.py
+++ todo_back/todo_back/urls.py
@@ -12,8 +12,8 @@ router = DefaultRouter()
 urlpatterns = [
     # Include the router's URL patterns
     path('admin/', admin.site.urls),
-    path('api/', include('Description.urls')),
-    path('accounts/', include('UserWork.urls')),
+    path('api/', include('todo_back.Description.urls')),
+    path('accounts/', include('todo_back.UserWork.urls')),
       
 ]
 
