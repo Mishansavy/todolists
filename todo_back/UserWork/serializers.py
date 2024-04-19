a9c3292e2fc160bb7a52b2f2b5d5f7febe23335e
Mishan Shah <smishan7@gmail.com>
2024-04-19 14:29:45 +0545

    solving deployment errors

--- todo_back/UserWork/serializers.py
+++ todo_back/UserWork/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import *
+from .models import CustomUser
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only = True, required=False)
