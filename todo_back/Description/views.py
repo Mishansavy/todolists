1ccea962a599bff75b520731cfca70c14c4a1a98
Mishan Shah <smishan7@gmail.com>
2024-04-19 13:59:23 +0545

    deploying backend on render again

--- todo_back/Description/views.py
+++ todo_back/Description/views.py
@@ -1,6 +1,6 @@
 # todolist/views.py
 from django.contrib.auth.models import User
-from serializers import *
+from .serializers import TodoItemSerializer,TodoItemSerializerUpdate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
