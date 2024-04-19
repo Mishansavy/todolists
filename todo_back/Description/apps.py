0fe506b90a7e2f1f87d67e1884d40130fe54d77d
Mishan Shah <smishan7@gmail.com>
2024-04-19 23:13:27 +0545

    deploy on render 9

--- todo_back/Description/apps.py
+++ todo_back/Description/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Description'
+    name = 'todo_back.Description'
