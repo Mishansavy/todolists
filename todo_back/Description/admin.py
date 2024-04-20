0ff68a6e371f2b5ef9088f62a3c9e8bdda5dc8ad
Mishan Shah <smishan7@gmail.com>
2024-04-20 09:39:20 +0545

    deleted all backend code from frontend branch

--- todo_back/Description/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import TodoItem
-from ..UserWork.models import CustomUser
-# Register your models here.
-admin.site.register(TodoItem)
-admin.site.register(CustomUser)
